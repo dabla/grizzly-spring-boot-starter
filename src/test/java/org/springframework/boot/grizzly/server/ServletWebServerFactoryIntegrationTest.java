0b9f35c2a57adbe22d493f25acb6f698a1056ed1
David Blain <david.blain@infrabel.be>
2019-05-28 09:19:23 +0200

    Moved ServletWebServerFactoryIntegrationTest to corresponding package

--- src/test/java/org/springframework/boot/grizzly/server/ServletWebServerFactoryIntegrationTest.java
+++ src/test/java/org/springframework/boot/grizzly/server/ServletWebServerFactoryIntegrationTest.java
@@ -1,10 +1,11 @@
-package org.springframework.boot.grizzly;
+package org.springframework.boot.grizzly.server;
 
 import javax.inject.Inject;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.grizzly.GrizzlyApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
