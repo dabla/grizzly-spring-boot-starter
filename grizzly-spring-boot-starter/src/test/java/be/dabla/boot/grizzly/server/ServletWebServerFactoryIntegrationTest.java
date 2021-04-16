3c0a4af728a92eafc792c893546cf29b1822a7e2
David Blain <info@dabla.be>
2021-04-16 10:28:31 +0200

    Tried to fix the ServletWebServerFactoryIntegrationTest

--- grizzly-spring-boot-starter/src/test/java/be/dabla/boot/grizzly/server/ServletWebServerFactoryIntegrationTest.java
+++ grizzly-spring-boot-starter/src/test/java/be/dabla/boot/grizzly/server/ServletWebServerFactoryIntegrationTest.java
@@ -2,9 +2,13 @@ package be.dabla.boot.grizzly.server;
 
 import be.dabla.boot.grizzly.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
+@SpringBootTest(webEnvironment = DEFINED_PORT)
 public class ServletWebServerFactoryIntegrationTest extends AbstractIntegrationTest {
     @Test
     public void getPort() {
